591b2454e81f6636dae3b14c2c01e3f76b49e74e
kim tran <icycleitaly@gmail.com>
2018-03-19 22:33:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,33 +1,33 @@
 function add(a,b){
-  return (a+b)
+  return (a+b);
 }
 
 function subtract(a,b){
-  return (a-b)
+  return (a-b);
 }
 
 function multiply(a,b){
-  return (a*b)
+  return (a*b);
 }
 
 function divide(a,b){
-  return (a/b)
+  return (a/b);
 }
 
 function inc(n){
-  return (n+=)
+  return (n+=);
 }
 
 function dec(n){
-  return n-=
+  return n-=;
 }
 
 function makeInt(n){
   if (parseInt(n) === 'number'){
-    return parseInt(n)
+    return parseInt(n);
   }
   if (isNaN(parseInt(n)) = true){
-    return true
+    return true;
 }
 
 
